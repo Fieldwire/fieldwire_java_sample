057e580082127ddc81c86cb14fff84d917f573a0
jayrave <jayanthan.raveendiran@gmail.com>
2023-11-01 23:42:51 -0700

    refactor `TokenManager` for reuse of response handling

--- app/src/net/fieldwire/TokenManager.java
+++ app/src/net/fieldwire/TokenManager.java
@@ -4,12 +4,14 @@ import com.google.gson.Gson;
 
 import net.fieldwire.endpoints.SuperEndpoint;
 import net.fieldwire.models.request.CreateJwt;
+import net.fieldwire.models.response.JwtResponse;
 
 import org.jetbrains.annotations.NotNull;
 
 import java.io.IOException;
 import java.util.function.Function;
 
+import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.Response;
 import retrofit2.Retrofit;
@@ -29,6 +31,13 @@ public class TokenManager {
         this.gson = gson;
     }
 
+    // `call` will be called with the access token. If the call made fails,
+    // because of bad access token (expired or invalid), this method will
+    // automatically retry & try the call again
+    //
+    // NOTE: make sure that the passed in function can be called multiple times
+    // without messing up any of your internal state (i.e., change your
+    // internal state only on a successful response from the server)
     public <T> T executeWithToken(Function<String, Call<T>> call) throws IOException {
         Response<T> response = call.apply(buildAuthzHeaderValue()).execute();
         if (!response.isSuccessful() && response.code() == 401) {
@@ -42,6 +51,33 @@ public class TokenManager {
             response = call.apply(buildAuthzHeaderValue()).execute();
         }
 
+        return handleResponse(response);
+    }
+
+    private void refresh() throws IOException {
+        // NOTE: In a multi-thread scenario, please make sure multiple threads
+        // aren't refreshing at the same time - let the first thread do it &
+        // for the other threads to use the newly acquired access token
+        // (otherwise, there will be rate limit issues). May we suggest looking
+        // into `Lock` for this? (a multi-process setup would require a more
+        // sophisticated system)
+
+        Response<JwtResponse> response = new Retrofit.Builder()
+                .baseUrl(SuperEndpoint.API_BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create(gson))
+                .build()
+                .create(SuperEndpoint.class)
+                .createAccessToken(new CreateJwt(this.refreshToken))
+                .execute();
+
+        // NOTE: please make sure to persist this somewhere outside the script
+        // so it can reused for the next invocation of the script (otherwise,
+        // there will be rate limit issues)
+        this.accessToken = handleResponse(response).access_token();
+    }
+
+    @NotNull
+    private <T> T handleResponse(Response<T> response) throws IOException {
         if (response.isSuccessful()) {
             return response.body();
         } else {
@@ -51,18 +87,6 @@ public class TokenManager {
                     response.errorBody().string()
             ));
         }
-    }
-
-    private void refresh() throws IOException {
-        this.accessToken = new Retrofit.Builder()
-                .baseUrl(SuperEndpoint.API_BASE_URL)
-                .addConverterFactory(GsonConverterFactory.create(gson))
-                .build()
-                .create(SuperEndpoint.class)
-                .createAccessToken(new CreateJwt(this.refreshToken))
-                .execute()
-                .body()
-                .access_token();
     }
 
     @NotNull
